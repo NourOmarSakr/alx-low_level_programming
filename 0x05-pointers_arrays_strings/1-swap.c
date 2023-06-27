32ea66224b4aade46cdc20c8449322c256b119d5
Nour Sakr <nour.van.sakr@gmail.com>
2023-06-27 16:17:22 -0700

    t

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - swaps 2 intigers
+ * @a: input parameter 1
+ * @b: input parameter 2
+ * Return: Nothing
+ */
+void swap_int(int *a, int *b)
+{
+        int temp;
+
+        temp = *a;
+        *a = *b;
+        *b = temp;
+}
