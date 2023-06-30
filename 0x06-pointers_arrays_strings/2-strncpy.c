9469b77a61530fc5b83e2c058c091bcf7517c487
Nour Sakr <nour.van.sakr@gmail.com>
2023-06-30 15:44:28 -0700

    Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,19 +2,19 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-    int x; 
+	int x;
 
-    for (x=0; x< n && src[x] != '\0'; x++)
-    {
-        dest[x] = src[x];
-    }
+	for (x = 0; x < n && src[x] != '\0'; x++)
+	{
+		dest[x] = src[x];
+	}
 
-    while (x<n)
-    {
-        dest[x] = '\0';
-	x++
-    }
+	while (x < n)
+	{
+		dest[x] = '\0';
+		x++
+	}
 
-   
-    return (dest);
+
+	return (dest);
 }
