6547a03bbc96b7f7626ca9915fa096e84d780485
Nour Sakr <nour.van.sakr@gmail.com>
2023-06-30 17:34:04 -0700

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,52 +1,35 @@
 #include "main.h"
 
 /**
- * is_separator - checks if a character is a separator of words
- * @c: the character to check
+ * cap_string - checks if a character is a separator of words
+ * @str: the character to check
  * Return: 1 if true, 0 if false
  */
-int is_separator(char c)
+
+char *cap_string(char *str)
 {
-	char sep[] = " \t\n,;.!?\"(){}";
-	int i;
+	int index = 0;
 
-	for (i = 0; sep[i] != '\0'; i++)
+	while (str[index])
 	{
-		if (c == sep[i])
-		{
-			return (1);
-		}
-	}
-	return (0);
-}
-
-/**
- * cap_string - capitalizes all words of a string
- * @s: the string to capitalize
- * Return: a pointer to the resulting string
- */
-char *cap_string(char *s)
-{
-	int i = 0, cap = 1;
-
-	while (s[i] != '\0')
-	{
-		if (is_separator(s[i]))
-		{
-			cap = 1;
-		}
-		else if (s[i] >= 'a' && s[i] <= 'z' && cap == 1)
-		{
-			s[i] -= ('a' - 'A');
-			cap = 0;
-		}
-		else if ((s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= 'a' && s[i] <= 'z'))
-		{
-			cap = 0;
-		}
-		/* else, do nothing */
-		i++;
-	}
-
-	return (s);
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++
+			if (str[index - 1] == ' ' ||
+			str[index - 1] == '\t' ||
+			str[index - 1] == '\n' ||
+			str[index - 1] == ',' ||
+			str[index - 1] == ';' ||
+			str[index - 1] == '.' ||
+			str[index - 1] == '!' ||
+			str[index - 1] == '?' ||
+			str[index - 1] == '"' ||
+			str[index - 1] == ')' ||
+			str[index - 1] == '(' ||
+			str[index - 1] == '}' ||
+			str[index - 1] == '{' || index == 0)
+			{
+				str[index] -= 32;
+				index++;
+			}
+		return (str);
 }
