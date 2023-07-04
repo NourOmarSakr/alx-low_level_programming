d3426cf2bd04cb23ec64e10674c939b61a2451f3
Nour Sakr <nour.van.sakr@gmail.com>
2023-07-04 13:08:10 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			for (k = j + 1; k < 10; k++)
+			{
+				int c1 = (int) (i + '0');
+				int c2 = (int) (j + '0');
+				int c3 = (int) (k + '0');
+
+				putchar((char) c1);
+				putchar((char) c2);
+				putchar((char) c3);
+
+				if (i != 7 || j != 8 || k != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	return (0);
+}
