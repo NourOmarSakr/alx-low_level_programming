128e045804646399f87b067e0924b23fcfe58eb1
Nour Sakr <nour.van.sakr@gmail.com>
2023-07-04 11:52:09 -0700

    My only C file Modification#1

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,6 +4,7 @@
 
 /* more headers goes there */
 /* betty style doc for function main goes there */
+
 int main(void)
 {
 	int n;
@@ -13,13 +14,13 @@ int main(void)
 
 	if (n > 0)
 	{
-		printf("is positive");
+		printf("%d is positive", n);
 	} else if (n < 0)
 	{
-		printf("is negative");
+		printf("%d is negative", n);
 	} else
 	{
-		printf("is zero");
+		printf("%d is zero", n);
 	}
 
 	return (0);
