08adc45b5c4ad31173bcab8e17751a02f5fd2f66
Nour Sakr <nour.van.sakr@gmail.com>
2023-07-04 11:06:14 -0700

    Task 5

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Error)
+ */
+int main(void)
+{
+	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, s, 59);
+
+	return (1);
+}
