af6c92b6efe0de893103004e3d58c6d343d3b6cf
Nour Sakr <nour.van.sakr@gmail.com>
2023-07-06 14:48:17 -0700

    Task Completed

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,17 +8,23 @@
  */
 int main(void)
 {
-	_puts_recursion("Puts with recursion");
-	return (0);
+    _puts_recursion("Puts with recursion");
+    return (0);
 }
 
+/**
+ * _puts_recursion - prints a string followed by a newline using recursion
+ * @s: the string to print
+ */
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	while (s[i] != '\0')
-	{
-		printf("%c", s[i]);
-		i++;
-	}
+    /* base case: if the string is empty, print a newline and return */
+    if (*s == '\0')
+    {
+        printf("\n");
+        return;
+    }
+    /* recursive case: print the first character and call the function again with the rest of the string */
+    printf("%c", *s);
+    _puts_recursion(s + 1);
 }
