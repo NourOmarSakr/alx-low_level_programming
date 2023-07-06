b85ab0bec6210780890183e60abf632ab733eea0
Nour Sakr <nour.van.sakr@gmail.com>
2023-07-06 14:33:21 -0700

    Task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_puts_recursion("Puts with recursion");
+	return (0);
+}
+
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		printf("%c", s[i]);
+		i++;
+	}
+}
