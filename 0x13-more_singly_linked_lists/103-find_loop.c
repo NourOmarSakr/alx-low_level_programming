6a53d3fb628aa099bc91151c3d858802f5687898
Nour Sakr <nour.van.sakr@gmail.com>
2023-08-01 17:09:02 -0700

    Task Completed

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -2,46 +2,18 @@
 
 /**
  * find_listint_loop - finds the loop in a linked list
- * @head: pointer to the first node of the list
- * Return: The address of the node where the loop starts
+ * @head: pointer to the head of the list
+ * Return: the address of the node where the loop starts, or NULL if no loop
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *slow, *fast;
+	listint_t *s = head, *f = head ? head->next : NULL;
 
-	if (head == NULL)
+	while (s && f && f->next && s != f)
+	s = s->next, f = f->next->next;
+	if (s != f)
 		return (NULL);
-
-	slow = head;
-	fast = head;
-
-	while (slow && fast && fast->next)
-	{
-		slow = slow->next;
-		fast = fast->next->next;
-		if (slow == fast)
-			return (find_start(head, slow));
-	}
-
-	return (NULL);
-}
-
-/**
- * find_start - finds the starting node of a loop in a linked list
- * @head: pointer to the first node of the list
- * @meet: pointer to the meeting node of slow and fast pointers
- * Return: pointer to the starting node of the loop
- */
-listint_t *find_start(listint_t *head, listint_t *meet)
-{
-	listint_t *start;
-
-	start = head;
-	while (start != meet)
-	{
-		start = start->next;
-		meet = meet->next;
-	}
-
-	return (start);
+	for (s = head; s != f; s = s->next, f = f->next)
+	;
+	return (s);
 }
