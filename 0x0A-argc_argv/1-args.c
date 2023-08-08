5f1ff1433bfaf32c3b73e0cd1dad3c8ba25e8dd5
NourOmarSakr <nour.van.sakr@gmail.com>
2023-08-08 11:50:48 -0700

    Task Modified

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file
+ * @argc: Number of arguments.
+ * @argv: Array of arguments.
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%i\n", argc);
+	return (0);
+}
