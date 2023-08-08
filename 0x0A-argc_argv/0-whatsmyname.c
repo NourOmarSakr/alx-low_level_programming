8a0deaf75407c6d961a73a97df3d5ab85ececbb8
NourOmarSakr <nour.van.sakr@gmail.com>
2023-08-08 11:20:10 -0700

    Task Completed

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file
+ * @argc: Number of arguments.
+ * @argv: Array of arguments.
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+}
