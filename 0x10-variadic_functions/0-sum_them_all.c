8c56434588fcf5b0b417412ac9d822ade627d27c
NourOmarSakr <nour.van.sakr@gmail.com>
2023-08-17 15:55:16 -0700

    Betty documentation style failed

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -4,12 +4,10 @@
 /**
  * sum_them_all - returns the sum of all its parameters
  * @n: the number of parameters
- * @… : the parameters to be summed
- *
- * Return: the sum of all parameters, or 0 if n == 0
-*/
-
-int sum_them_all(const unsigned int n, …)
+ * @...: the variadic parameters
+ * Return: the sum of the parameters
+ */
+int sum_them_all(const unsigned int n, ...)
 {
 	va_list args;
 	unsigned int i;
@@ -19,12 +17,9 @@ int sum_them_all(const unsigned int n, …)
 		return (0);
 
 	va_start(args, n);
-
 	for (i = 0; i < n; i++)
 		sum += va_arg(args, int);
 
 	va_end(args);
-
 	return (sum);
-
 }
