77508f057ab44cb293292400e7b91cbaa6063a1f
NourOmarSakr <nour.van.sakr@gmail.com>
2023-08-24 19:49:31 -0700

    Task Completed

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,75 +1,28 @@
 #include "lists.h"
-
-/**
- * add_node - adds a new node at the beginning of a list_t list
- * @head: pointer to the head of the list
- * @str: string to be added to the new node
- * Return: pointer to the new node, or NULL if it fails
- */
-list_t *add_node(list_t **head, const char *str)
-{
-	list_t *new_node;
-	unsigned int len;
-
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-
-	len = 0;
-	while (str[len])
-		len++;
-
-	new_node->len = len;
-	new_node->next = *head;
-
-	*head = new_node;
-
-	return (new_node);
-}
+#include <stdio.h>
 
 /**
  * print_list - prints all the elements of a list_t list
  * @h: pointer to the head of the list
- * Return: number of nodes in the list
+ * Return: the number of nodes
  */
 size_t print_list(const list_t *h)
 {
-	size_t count;
+	size_t count = 0;
+	const list_t *current = h;
 
-	count = 0;
-	while (h != NULL)
+	while (current != NULL)
 	{
-	printf("[%u] %s\n", h->len, h->str);
-
-		h = h->next;
+		if (current->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", current->len, current->str);
+		}
 		count++;
+		current = current->next;
 	}
-
 	return (count);
-}
-
-/**
- * free_list - frees a list_t list
- * @head: pointer to the head of the list
- */
-void free_list(list_t *head)
-{
-	list_t *temp;
-
-	while (head != NULL)
-	{
-		temp = head->next;
-
-		free(head->str);
-		free(head);
-
-		head = temp;
-	}
 }
